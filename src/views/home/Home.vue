8ac9c40d4565e9ae02d4c5dd12b797ecdb627de9
Keabuls <asonmezkaan@hotmail.com>
2025-07-14 19:14:46 +0300

    home page fetching products

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,5 +3,29 @@ Home View
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
+import productService from '@/services/productService'
+const products = ref([])
+const loading = ref(false)
 
+onMounted(() =>{
+    fetchProducts();
+})
+
+const fetchProducts = async () => {
+    try{
+        loading.value = true;
+        products.value = await productService.getAllProducts()
+        loading.value = false;
+        console.log(products.value)
+
+    }
+    catch(err){
+        console.log(err)
+        loading.value=false;
+    }
+    finally{    
+        loading.value=false;
+    }
+}
 </script>
