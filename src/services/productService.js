ad8f19ed7372e3f1c609cd85eb72f57b038cb78c
Keabuls <asonmezkaan@hotmail.com>
2025-07-16 17:36:55 +0300

    signIn & SignUp

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,44 +1,47 @@
-import {db} from '@/utility/firebaseConfig.js';
-import { collection, addDoc, getDocs, getDoc, doc, updateDoc, deleteDoc} from 'firebase/firestore';
+import { db } from '@/utility/firebaseConfig.js'
+import { collection, addDoc, getDocs, getDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore'
 
-const productCollection = collection(db,'products');
-
-
-
-
+const productCollection = collection(db, 'products')
+const userInfoCollection = collection(db, 'userInfo')
 
 export default {
-    async createProduct(productData){
-       const docRef = await addDoc(productCollection, productData);
-       return {id: docRef.id, ...productData};
-    },
+  async createUser(userInfo) {
+    const docRef = await addDoc(userInfoCollection, userInfo)
+    return { id: docRef.id, ...userInfo }
+  },
 
-    async getAllProducts() {
-    const snapshot = await getDocs(productCollection);
-    return snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()}))
-    },
+  
 
 
-    async getDocById(id) {
-        const docRef = doc(db, 'products', id);
-        const snapshot = await getDoc(docRef);
-        if (snapshot.exists()) {
-            return {id: snapshot.id, ...snapshot.data()};
-        } else {
-            throw new Error('No such document!');
-        }
-    },
 
-    async updateProduct(id, productData) {
-        const docRef = doc(db, 'products', id);
-        await updateDoc(docRef, productData);
-        return {id, ...productData};
-    },
-    async deleteProduct(id) {
-        const docRef = doc(db, 'products', id);
-        await deleteDoc(docRef);
-        return id;
-    },
+  async createProduct(productData) {
+    const docRef = await addDoc(productCollection, productData)
+    return { id: docRef.id, ...productData }
+  },
+
+  async getAllProducts() {
+    const snapshot = await getDocs(productCollection)
+    return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+  },
+
+  async getDocById(id) {
+    const docRef = doc(db, 'products', id)
+    const snapshot = await getDoc(docRef)
+    if (snapshot.exists()) {
+      return { id: snapshot.id, ...snapshot.data() }
+    } else {
+      throw new Error('No such document!')
+    }
+  },
+
+  async updateProduct(id, productData) {
+    const docRef = doc(db, 'products', id)
+    await updateDoc(docRef, productData)
+    return { id, ...productData }
+  },
+  async deleteProduct(id) {
+    const docRef = doc(db, 'products', id)
+    await deleteDoc(docRef)
+    return id
+  },
 }
-
-
--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -1,7 +1,73 @@
 <template>
-    Sing In View
+    <div class="container py-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6 col-lg-4 border">
+        <div class="shadow-sm">
+          <div class="card-body p-4">
+            <h4 class="text-center mb-4">Sign In</h4>
+            <form @submit.prevent="handleSignIn">
+              <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input v-model="form.email" type="email" class="form-control" id="email" required />
+              </div>
+              <div class="mb-4">
+                <label for="password" class="form-label">Password</label>
+                <input
+                v-model="form.password"
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  required
+                />
+              </div>
+              <button type="submit" class="btn btn-success w-100">
+                <span v-if="loading" class="spinner-border spinner-border-sm me-2"></span>
+                Sign In
+              </button>
+              <div v-if="error" class="alert alert-danger mt-3 mb-0">{{ error }}</div>
+            </form>
+            <p class="mt-3 form-label text-center">
+              Don't have an account?
+              <router-link :to="{name:APP_ROUTE_NAMES.SING_UP}">Register here</router-link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import {APP_ROUTE_NAMES} from '@/constants/routeNames.js';
+import { ref,reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import { useSwal } from '@/utility/useSwal';
+
+const { showSuccess, showError } = useSwal();
+const router = useRouter();
+
+const error = ref('');
+const loading = ref(false);
+const form = reactive({
+  email: '',
+  password: ''
+});
+
+
+const handleSignIn = async () =>{
+    try{
+        error.value = '';
+        console.log(form);
+        router.push({name:APP_ROUTE_NAMES.HOME});
+
+    }
+    catch(error){
+    error.value = error.message;
+    showError(error.message);
+    }
+    finally{
+    loading.value = false;
+    };
+}
 
 </script>
