48a9fdc1b268c2a09ba264a94917defba0140efb
Keabuls <asonmezkaan@hotmail.com>
2025-07-17 12:46:02 +0300

    authStore and firabase Config

--- src/utility/firebaseConfig.js
+++ src/utility/firebaseConfig.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 import { FIREBASE_API } from '@/constants/firebaseInfo.js';
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export {app as firebaseApp, db }; 
+const auth = getAuth(app);
+export {app as firebaseApp, db, auth}; 
--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -1,11 +1,11 @@
 <template>
-     <div class="container py-5 my-3">
+  <div class="container py-5 my-3" >
     <div class="row justify-content-center">
       <div class="col-md-6 col-lg-4 border rounded">
         <div class="shadow-sm">
           <div class="card-body p-4">
             <h4 class="text-center mb-4">Create Account</h4>
-            <form @submit.prevent="handleSignUp" >
+            <form @submit.prevent="handleSignUp">
               <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
                 <input v-model="form.email" type="email" class="form-control" id="email" required />
@@ -13,22 +13,22 @@
               <div class="mb-4">
                 <label for="password" class="form-label">Password</label>
                 <input
-                    v-model="form.password"
-                    type="password"
-                    class="form-control"
-                    id="password"
-                    required
+                  v-model="form.password"
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  required
                 />
               </div>
               <button type="submit" class="btn btn-success w-100">
                 <span v-if="loading" class="spinner-border spinner-border-sm me-2"></span>
                 Create Account
               </button>
-              <div v-if="error" class="alert alert-danger mt-3 mb-0">{{error}}</div>
+              <div v-if="error" class="alert alert-danger mt-3 mb-0">{{ error }}</div>
             </form>
             <p class="mt-3 form-label text-center">
               Already have an account?
-              <router-link :to="{name:APP_ROUTE_NAMES.SING_IN}">Login here</router-link>
+              <router-link :to="{ name: APP_ROUTE_NAMES.SING_IN }">Login here</router-link>
             </p>
           </div>
         </div>
@@ -38,38 +38,32 @@
 </template>
 
 <script setup>
-import {APP_ROUTE_NAMES} from '@/constants/routeNames.js';
-import { ref,reactive } from 'vue';
-import { useRouter } from 'vue-router';
-import { useSwal } from '@/utility/useSwal';
+import { APP_ROUTE_NAMES } from '@/constants/routeNames.js'
+import { ref, reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import { useSwal } from '@/utility/useSwal'
 
-const { showSuccess, showError } = useSwal();
-const router = useRouter();
+const { showSuccess, showError } = useSwal()
+const router = useRouter()
 
-const error = ref('');
-const loading = ref(false);
+const error = ref('')
+const loading = ref(false)
 const form = reactive({
   email: '',
-  password: ''
-});
+  password: '',
+})
 
-
-const handleSignUp = async () =>{
-    try{
-        error.value = '';
-        console.log(form);
-        showSuccess('Account created successfully');
-
-
-
-    }
-    catch(error){
-    error.value = error.message;
-    showError(error.message);
-    }
-    finally{
-    loading.value = false;
-    };
+const handleSignUp = async () => {
+  try {
+    error.value = ''
+    console.log(form)
+    showSuccess('Account created successfully')
+  } catch (error) {
+    error.value = error.message
+    showError(error.message)
+  } finally {
+    loading.value = false
+  }
 }
-
 </script>
+
